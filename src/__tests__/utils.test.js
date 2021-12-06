169513cabcc951c06f1d3a49b900dd2a82af8d1c
davin <82827878+davin67@users.noreply.github.com>
2021-12-05 20:47:02 -0800

    Initialize ceorourke/API-1347

---
+++ src/__tests__/utils.test.js
@@ -0,0 +1,72 @@
+import { sortPages, parseMarkdown } from "../utils";
+
+const PAGE_ONE = { context: { title: "Test", sidebar_order: 5 } };
+const PAGE_TWO = { context: { title: "Test Two", sidebar_order: 10 } };
+const PAGE_THREE = { context: { title: "Test Three" } };
+
+describe("sortPages", () => {
+  it("sorts without extractor", () => {
+    const result = sortPages([PAGE_ONE, PAGE_TWO, PAGE_THREE]);
+    expect(result[0]).toBe(PAGE_ONE);
+    expect(result[1]).toBe(PAGE_THREE);
+    expect(result[2]).toBe(PAGE_TWO);
+  });
+
+  it("sorts with extractor", () => {
+    const result = sortPages(
+      [{ node: PAGE_ONE }, { node: PAGE_TWO }, { node: PAGE_THREE }],
+      n => n.node
+    );
+    expect(result[0].node).toBe(PAGE_ONE);
+    expect(result[1].node).toBe(PAGE_THREE);
+    expect(result[2].node).toBe(PAGE_TWO);
+  });
+});
+
+describe("parseMarkdown", () => {
+  it("converts a single code formatted string in a string", () => {
+    const result = parseMarkdown(
+      "The `quick` brown fox jumps over the lazy dog."
+    );
+    expect(result).toBe(
+      "The <code>quick</code> brown fox jumps over the lazy dog."
+    );
+  });
+
+  it("coverts multiple code formatted strings in a string", () => {
+    const result = parseMarkdown(
+      "The `quick` `brown` fox `jumps` over the lazy dog."
+    );
+    expect(result).toBe(
+      "The <code>quick</code> <code>brown</code> fox <code>jumps</code> over the lazy dog."
+    );
+  });
+
+  it("converts multiple backticks as code element", () => {
+    const result = parseMarkdown(
+      'Possible values are: ``""`` (disable),``"24h"``, ``"14d"``'
+    );
+
+    expect(result).toBe(
+      'Possible values are: <code>""</code> (disable),<code>"24h"</code>, <code>"14d"</code>'
+    );
+  });
+  it("converts markdown link to html", () => {
+    const result = parseMarkdown(
+      "This can be retrieved from the [beforeSend callback](https://docs.sentry.io/platforms/javascript/configuration/filtering/#using-beforesend)."
+    );
+
+    expect(result).toBe(
+      'This can be retrieved from the <a href="https://docs.sentry.io/platforms/javascript/configuration/filtering/#using-beforesend">beforeSend callback</a>.'
+    );
+  });
+  it("converts markdown link and code formatted strings to html", () => {
+    const result = parseMarkdown(
+      "This `example` can be retrieved from the [beforeSend callback](https://docs.sentry.io/platforms/javascript/configuration/filtering/#using-beforesend)."
+    );
+
+    expect(result).toBe(
+      'This <code>example</code> can be retrieved from the <a href="https://docs.sentry.io/platforms/javascript/configuration/filtering/#using-beforesend">beforeSend callback</a>.'
+    );
+  });
+});
