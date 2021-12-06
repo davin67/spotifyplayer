9dd9145aab5b038446d518d998d2729859d9ce0a
davin <82827878+davin67@users.noreply.github.com>
2021-12-05 20:45:50 -0800

    Initialize revert-2147-fix/gatsby-apm-warning

---
+++ src/components/markdown.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { MDXProvider } from "@mdx-js/react";
+import { MDXRenderer } from "gatsby-plugin-mdx";
+
+import "prismjs/themes/prism-tomorrow.css";
+
+import Alert from "./alert";
+import Break from "./break";
+import SmartLink from "./smartLink";
+import CodeBlock from "./codeBlock";
+import CodeTabs from "./codeTabs";
+import ConfigKey from "./configKey";
+import GuideGrid from "./guideGrid";
+import JsCdnTag from "./jsCdnTag";
+import PageGrid from "./pageGrid";
+import ParamTable from "./paramTable";
+import PlatformContent from "./platformContent";
+import PlatformSection from "./platformSection";
+
+const mdxComponents = {
+  Alert,
+  a: SmartLink,
+  Break,
+  CodeBlock,
+  CodeTabs,
+  ConfigKey,
+  GuideGrid,
+  JsCdnTag,
+  Link: SmartLink,
+  PageGrid,
+  ParamTable,
+  PlatformContent,
+  PlatformSection,
+};
+
+export default ({ value }) => {
+  return (
+    <MDXProvider components={mdxComponents}>
+      <MDXRenderer>{value}</MDXRenderer>
+    </MDXProvider>
+  );
+};
